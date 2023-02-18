0f4ba872f6909468dc06cb5edd6febe3c8f422b0
Noah Stuertz <noah.stuertz@gmail.com>
2023-02-18 11:51:49 +0100

    added Footer Components

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import FooterItem from './FooterItem.vue'
+</script>
+
+<template>
+    <div class="container">
+        <footer class="py-3 my-3">
+            <ul class="nav pb-3 mb-3 justify-content-center border-bottom">
+                <FooterItem link="/" title="Home" />
+                <FooterItem link="/about" title="About" />
+                <FooterItem link="/imprint" title="Imprint" />
+            </ul>
+            <p class="text-center text-muted">
+                2023 - Bakta
+            </p>
+        </footer>
+    </div>
+</template>
---
+++ src/components/FooterItem.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+    props: {
+        link: { type: String, required: true },
+        title: String
+    }
+}
+</script>
+
+<template>
+    <li class="navitem">
+        <router-link class="nav-link px-2 text-muted" :to="link">{{ title }}</router-link>
+    </li>
+</template>
+
+<style></style>
