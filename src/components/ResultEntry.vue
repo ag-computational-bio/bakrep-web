9a13e168f32b365c24e23912f2ba28be5f543e18
Noah Stuertz <noah.stuertz@gmail.com>
2023-02-20 10:54:23 +0100

    added components to display results

---
+++ src/components/ResultEntry.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+export default {
+    props: {
+        entry: Object
+    },
+}
+</script>
+
+<template>
+    <td scope="row">{{ entry.id }}</td>
+    <td>{{ entry.GC }}</td>
+    <td>{{ entry.Contigs }}</td>
+    <td>{{ entry.Name }}</td>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/ResultTable.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import ResultEntry from './ResultEntry.vue';
+
+export default {
+    props: {
+        entries: Object
+    },
+    components: { ResultEntry }
+}
+</script>
+
+<template>
+    <div class="py-3 my-5 rounded bg-body-tertiary">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">GC</th>
+                    <th scope="col">Contigs</th>
+                    <th scope="col">Name</th>
+                </tr>
+
+            </thead>
+            <tbody>
+                <tr v-for="entry in entries" :key="entry.id">
+                    <ResultEntry :entry="entry" />
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style></style>
