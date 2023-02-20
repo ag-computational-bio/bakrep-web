723c29f5993bce7532ae780a13e0f30e67cd6193
Noah Stuertz <noah.stuertz@gmail.com>
2023-02-20 10:52:31 +0100

    added search inputs

--- src/components/SearchWidget.vue
+++ src/components/SearchWidget.vue
@@ -1,17 +1,20 @@
-<script lang="ts">
-export default {
-  data() {
-    return {
-      greeting: 'Hello World!'
-    }
-  }
-}
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const searchInput = ref("")
 </script>
 
+<!-- 
+  An initial Searchwidget, will need to be refined depending on the search parameters and language
+ -->
+
 <template>
-  
+  <div class="p-3 rounded bg-body-tertiary">
+    <div class="input-group">
+      <button class="btn btn-outline-secondary" type="button" id="button-search">Search</button>
+      <input type="text" v-model.lazy="searchInput" placeholder="search bakrep for..." class="form-control" aria-describedby="button-search" aria-label="Search Button"/>
+    </div>
+  </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
