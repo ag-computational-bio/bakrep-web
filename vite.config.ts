6d82a25740ae5927ba6e0dd51b7f2d54149f6e2a
Noah Stuertz <noah.stuertz@gmail.com>
2023-02-20 13:45:41 +0100

    added polling support for WSL development

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
