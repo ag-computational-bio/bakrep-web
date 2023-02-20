55b9a793ddde52e00a9454846678881b8de860a5
Noah Stuertz <noah.stuertz@gmail.com>
2023-02-20 14:30:39 +0100

    added data binding to the entries

--- src/components/SearchWidget.vue
+++ src/components/SearchWidget.vue
@@ -1,7 +1,43 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const searchInput = ref("")
+<script lang="ts">
+export default {
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      searchInput: ""
+    }
+  },
+  methods: {
+    searchRepository(input: string) {
+      let testResults = [{
+    "id": 2,
+    "GC": 0.75,
+    "Contigs": 175,
+    "Name": "Decumaria L."
+  }, {
+    "id": 3,
+    "GC": 0.23,
+    "Contigs": 171,
+    "Name": "Avicennia germinans (L.) L."
+  }, {
+    "id": 4,
+    "GC": 0.40,
+    "Contigs": 19,
+    "Name": "Cetraria ericetorum Opiz ssp. reticulata (Rasanen) Karnefelt"
+  }, {
+    "id": 5,
+    "GC": 0.61,
+    "Contigs": 66,
+    "Name": "Rudbeckia laciniata L. var. laciniata"
+  }, {
+    "id": 6,
+    "GC": 0.49,
+    "Contigs": 170,
+    "Name": "Tetraclinis articulata (Vahl) Masters"
+  }]
+      this.$emit('update:modelValue', testResults)
+    }
+  }
+}
 </script>
 
 <!-- 
@@ -11,7 +47,7 @@ const searchInput = ref("")
 <template>
   <div class="p-3 rounded bg-body-tertiary">
     <div class="input-group">
-      <button class="btn btn-outline-secondary" type="button" id="button-search">Search</button>
+      <button @click="searchRepository(searchInput)" class="btn btn-outline-secondary" type="button" id="button-search">Search</button>
       <input type="text" v-model.lazy="searchInput" placeholder="search bakrep for..." class="form-control" aria-describedby="button-search" aria-label="Search Button"/>
     </div>
   </div>
