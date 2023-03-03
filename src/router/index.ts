1395be35c650c3a7cd976a1fc1594d9074b4ad94
Noah Stürtz <noah.stuertz@gmail.com>
2023-03-03 16:34:44 +0100

    routed search to home

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,7 +25,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/SearchView.vue"),
+      component: () => import("../views/HomeView.vue"),
     },
     {
       path: "/imprint",
