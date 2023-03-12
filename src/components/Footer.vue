a6444c57d2b1a66ec02e12c63428344b1aecd157
Noah Stuertz <noah.stuertz@gmail.com>
2023-03-12 22:50:54 +0100

    inlined footeritem

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import FooterItem from './FooterItem.vue'
 </script>
 
 <template>
     <div class="container">
         <footer class="py-3 my-3">
             <ul class="nav pb-3 mb-3 justify-content-center border-bottom">
-                <FooterItem href="/" text="Home" />
-                <FooterItem href="/about" text="About" />
-                <FooterItem href="/imprint" text="Imprint" />
+                <li class="navitem">
+                    <router-link class="nav-link px-2 text-muted" to="/">Home</router-link>
+                </li>
+                <li class="navitem">
+                    <router-link class="nav-link px-2 text-muted" to="/about">About</router-link>
+                </li>
+                <li class="navitem">
+                    <router-link class="nav-link px-2 text-muted" to="/imprint">Imprint</router-link>
+                </li>
             </ul>
             <p class="text-center text-muted">
                 2023 - Bakta
