38ea9f101e11a426b4a9faa51b7112c71ec97bc3
Noah Stürtz <noah.stuertz@gmail.com>
2023-03-13 10:00:04 +0100

    removed polling

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,6 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true
-    }
-  },
   plugins: [vue()],
   resolve: {
     alias: {
