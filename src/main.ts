29f11f66d986eef0b3198d882c024bb98578fbf4
Noah Stürtz <noah.stuertz@gmail.com>
2023-03-23 12:23:38 +0100

    merged main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-
+import "./assets/main.css";
 
 const app = createApp(App);
 
