03d70e572a19db3e4c13af1c1f0ba59edd5ca744
Noah Stürtz <noah.stuertz@gmail.com>
2023-05-08 17:13:26 +0200

    enable gz support

--- src/Api.ts
+++ src/Api.ts
@@ -1,3 +1,5 @@
+import { inflate } from 'pako';
+
 const BASEURL = 'http://localhost:8081/';
 function getDatasetById(id: string) {
   let dataset: Promise<Dataset> = Promise.resolve(api<Dataset>(BASEURL + id))
@@ -10,7 +12,7 @@ function getDatasetById(id: string) {
 }
 
 function getResultData(url: string): any {
-  return Promise.resolve(api<any>(url))
+  return Promise.resolve(apiGzip<any>(url))
     .then((res) => {
       return res
     })
@@ -18,13 +20,26 @@ function getResultData(url: string): any {
 
 function api<T>(url: string): Promise<T> {
   return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json<T>();
-    })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json() as Promise<T>;
+  })
+}
 
+function apiGzip<T>(url: string): Promise<T> {
+  return fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.arrayBuffer();
+  })
+  .then(arrayBuffer => {
+    let result = inflate(arrayBuffer, { to: 'string'})
+    return JSON.parse(result) as Promise<T>
+  })
 }
 
 export { getDatasetById }
