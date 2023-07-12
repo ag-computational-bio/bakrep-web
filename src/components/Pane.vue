999e3e0dff5e95128d921b95587a315b53a755c6
Noah Stürtz <noah.stuertz@gmail.com>
2023-07-12 05:48:30 +0200

    Optimize Pane

--- src/components/Pane.vue
+++ src/components/Pane.vue
@@ -2,34 +2,34 @@
 import { ref } from 'vue';
 import Panetab from './Panetab.vue';
 
+type Action = {
+    title: string
+    href: object
+}
+
 let props = defineProps({
-    items: { type: Object, required: true },
-    action: { type: Object, required: false }
+    items: { type: Array<String>, required: true },
+    action: { type: Object, required: true }
 })
 
 let active = ref(props.items[0])
 
-function isActive(item: Object) {
-    return active === item
-}
 </script>
 
 <template>
     <!-- Tabbed Navigation -->
-    <div class="shadow-sm">
-        <ul class="nav nav-tabs">
-            <Panetab @click="active = item" :name="item" :active="isActive(item)" v-for="item in items" />
+    <div class="shadow-sm p-4">
+        <ul class="nav nav-pills">
+            <Panetab @click="active = item" :name="item" :active="active == item" v-for="item in items" />
             <li class="ms-auto" v-if="action">
                 <!-- @TODO: Figure out how to directly download an entry, json? -->
                 <a class="btn btn-primary" href="#">{{ action.title }}</a>
             </li>
         </ul>
         <div class="tab-content">
-            <div class="tab-pane" :class="{ active: isActive(item)}" v-for="item in items">
+            <div class="tab-pane" :class="{ 'active': active === item}" v-for="item in items">
                 <slot :name=item></slot>
             </div>
         </div>
     </div>
-
-    <!-- Tabbed Content -->
 </template> 
