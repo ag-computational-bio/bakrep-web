90b4e55227c82679270b28c8e86cedf98170f51c
Noah Stürtz <noah.stuertz@gmail.com>
2023-07-12 05:46:17 +0200

    Add Feature Type

--- src/Types.d.ts
+++ src/Types.d.ts
@@ -10,3 +10,34 @@ type Dataset = {
     id: string
     results: Array<Result>
 }
+
+type Feature = {
+    type: string,
+    contig: string,
+    start: number,
+    stop: number,
+    strand: string,
+    gene: any,
+    product: string
+    length: number
+}
+
+type Annotation = {
+    genome: {
+        genus: string,
+        species: string,
+        strain: string,
+        complete: boolean,
+        gram: string,
+        translation_table: string
+    },
+    stats: {
+        no_sequence: number,
+        size: number,
+        gc: number,
+        n_ratio: number,
+        n50: number,
+        coding_ratio: number
+    }
+    features: Array<Feature>
+}
