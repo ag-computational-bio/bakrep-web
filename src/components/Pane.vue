60d21ca2c7c4c2e27a08661823bdf31772188a82
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2023-08-22 17:56:26 +0200

    Fix bug: Every pane is loaded multiple times

--- src/components/Pane.vue
+++ src/components/Pane.vue
@@ -1,46 +1,50 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
-import Panetab from './Panetab.vue';
+import { onMounted, ref } from "vue";
+import Panetab from "./Panetab.vue";
 
 type Action = {
-    title: string
-    href: object
-}
+  title: string;
+  href: object;
+};
 
 let props = defineProps({
-    items: { type: Array<string>, required: true },
-    action: { type: Object, required: true }
-})
+  items: { type: Array<string>, required: true },
+  action: { type: Object, required: true },
+});
 
+const emits = defineEmits(["update:value"]);
 
-const emits = defineEmits(['update:value'])
-
-let value = ref(props.items[0])
+let value = ref(props.items[0]);
 
 onMounted(() => {
-    emits('update:value', value)
-})
-
+  emits("update:value", value);
+});
 </script>
 
 <template>
-    <!-- Tabbed Navigation -->
-    <div class="shadow-sm p-4">
-        <ul class="nav nav-pills py-3">
-            <Panetab @click="value = item; $emit('update:value', item)" :name="item" :active="value == item"
-                v-for="item in items" />
+  <!-- Tabbed Navigation -->
+  <div class="shadow-sm p-4">
+    <ul class="nav nav-pills py-3">
+      <Panetab
+        @click="
+          value = item;
+          $emit('update:value', item);
+        "
+        :name="item"
+        :active="value == item"
+        v-for="item in items"
+        :key="item"
+      />
 
-            <slot name="action">
-                <li class="ms-auto" v-if="action">
-                    <!-- @TODO: Figure out how to directly download an entry, json? -->
-                    <a class="btn btn-primary" href="#">{{ action.title }}</a>
-                </li>
-            </slot>
-        </ul>
-        <div class="tab-content">
-            <div class="tab-pane" :class="{ 'active': value === item }" v-for="item in items">
-                <slot></slot>
-            </div>
-        </div>
+      <slot name="action">
+        <li class="ms-auto" v-if="action">
+          <!-- @TODO: Figure out how to directly download an entry, json? -->
+          <a class="btn btn-primary" href="#">{{ action.title }}</a>
+        </li>
+      </slot>
+    </ul>
+    <div class="tab-content">
+      <slot></slot>
     </div>
-</template> 
+  </div>
+</template>
