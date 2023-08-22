fbb92e9abeaac4d98781ecfb4267a1c12e4049d4
Noah Stürtz <noah.stuertz@gmail.com>
2023-08-22 10:52:13 +0200

    add browse view

---
+++ src/views/BrowseView.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ResultTable from "@/components/ResultTable.vue";
+
+const entries = ref([{
+    "id": "SAMD00000344",
+    "GC": 0.75,
+    "Contigs": 175,
+    "Length": 2359987,
+    "Name": "Decumaria L."
+  }, {
+    "id": "SAMD00000344",
+    "GC": 0.23,
+    "Contigs": 171,
+    "Length": 2359987,
+    "Name": "Avicennia germinans (L.) L."
+  }, {
+    "id": "SAMD00000344",
+    "GC": 0.40,
+    "Contigs": 19,
+    "Length": 2359987,
+    "Name": "Cetraria ericetorum Opiz ssp. reticulata (Rasanen) Karnefelt"
+  }, {
+    "id": "SAMD00000344",
+    "GC": 0.61,
+    "Contigs": 66,
+    "Length": 2359987,
+    "Name": "Rudbeckia laciniata L. var. laciniata"
+  }, {
+    "id": "SAMD00000344",
+    "GC": 0.49,
+    "Contigs": 170,
+    "Length": 2359987,
+    "Name": "Tetraclinis articulata (Vahl) Masters"
+  }
+])
+
+</script>
+
+<template>
+  <main class="container pt-5">
+
+    <div class="row">
+      <h2>Browse the Genome Databank</h2>
+    </div>
+
+    <div class="row mt-3 mb-1">
+      <h3>Entries:</h3>
+    </div>
+
+    <div class="row">
+      <ResultTable :entries=entries />
+    </div>
+  </main>
+</template>
+
+<style>
+</style>
