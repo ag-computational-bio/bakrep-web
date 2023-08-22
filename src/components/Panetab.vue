ad545ca4032d913f304f8af0f4100bfb89a182a6
Noah Stürtz <noah.stuertz@gmail.com>
2023-08-22 10:51:27 +0200

    wip: add instant choice for pane

--- src/components/Panetab.vue
+++ src/components/Panetab.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-defineProps({
+let props = defineProps({
     name: { type: String, required: true },
     active: { type: Boolean, required: false }
 })
+
+let title = props.name.charAt(0).toLocaleUpperCase() + props.name.slice(1)
 </script>
 
 <template>
     <li class="nav-item">
-        <a class="nav-link" :class="{active}" href="#">{{ name }}</a>
+        <a class="nav-link" :class="{active}" href="#">{{ title }}</a>
     </li>
 </template> 
