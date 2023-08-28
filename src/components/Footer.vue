82aa45f90d539107a53b6698baabe01ee329e052
Noah Stürtz <noah.stuertz@gmail.com>
2023-08-28 20:47:19 +0200

    add some responsiveness

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,41 +1,28 @@
-<script setup lang="ts">
-</script>
-
 <template>
-    <div class="container">
-        <footer class="py-3 my-3">
-            <div class="d-flex flex-row">
-                <a href="https://www.uni-giessen.de/de/fbz/fb08/Inst/bioinformatik/" target="_blank">
-                    <img
-                        src="@/assets/images/CB.AG_Logo.svg"
-                        height="200"
-                        width="350"
-                    />
-                </a>
-                <a href="">
-                    <img
-                        src="@/assets/images/De.NBI_Logo.svg"
-                        width="350"
-                        height="160"
-                    />
-                </a>
-                <div class="mx-5 vr"></div>
-                <ul class="nav pb-3 mb-3 justify-content-center border-bottom d-flex flex-column">
-                    <li class="navitem">
-                        <router-link class="nav-link px-2 text-muted" to="/">Home</router-link>
-                    </li>
-                    <li class="navitem">
-                        <router-link class="nav-link px-2 text-muted" to="/about">About</router-link>
-                    </li>
-                    <li class="navitem">
-                        <router-link class="nav-link px-2 text-muted" to="/imprint">Imprint</router-link>
-                    </li>
-                </ul>
-            </div>
-            
-            <p class="text-center text-muted">
-                2023 - Bakta
-            </p>
-        </footer>
-    </div>
+  <div class="container">
+    <footer class="py-3 my-3">
+      <div class="d-flex flex-column flex-xl-row justify-content-center">
+        <a href="https://www.uni-giessen.de/de/fbz/fb08/Inst/bioinformatik/" target="_blank">
+          <img src="@/assets/images/CB.AG_Logo.svg" height="200" width="350" />
+        </a>
+        <a href="">
+          <img src="@/assets/images/De.NBI_Logo.svg" width="350" height="160" />
+        </a>
+        <div class="mx-5 vr justify-self-center"></div>
+        <ul class="nav pb-3 mb-3 justify-content-center flex-xl-column flex-row">
+          <li class="navitem">
+            <router-link class="nav-link px-2 text-muted" to="/">Home</router-link>
+          </li>
+          <li class="navitem">
+            <router-link class="nav-link px-2 text-muted" to="/about">About</router-link>
+          </li>
+          <li class="navitem">
+            <router-link class="nav-link px-2 text-muted" to="/imprint">Imprint</router-link>
+          </li>
+        </ul>
+      </div>
+
+      <p class="text-center text-muted">2023 - Bakta</p>
+    </footer>
+  </div>
 </template>
