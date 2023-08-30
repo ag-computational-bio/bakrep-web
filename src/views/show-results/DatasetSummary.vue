e752c2d38573846c6d85756435a578656cd96ed4
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2023-08-30 10:05:27 +0200

    update summary

--- src/views/show-results/DatasetSummary.vue
+++ src/views/show-results/DatasetSummary.vue
@@ -1,37 +1,37 @@
 <template>
   <table v-if="annotation" class="table statstable">
     <tr>
-      <th scope="row">Genus</th>
+      <th scope="row">Genus:</th>
       <td>{{ annotation.genome.genus }}</td>
     </tr>
     <tr>
-      <th scope="row">Species</th>
+      <th scope="row">Species:</th>
       <td>{{ annotation.genome.species }}</td>
     </tr>
     <tr>
-      <th scope="row">Strain</th>
+      <th scope="row">Strain:</th>
       <td>{{ annotation.genome.strain }}</td>
     </tr>
     <tr>
-      <th scope="row">Complete</th>
+      <th scope="row">Complete:</th>
       <td>{{ annotation.genome.complete }}</td>
     </tr>
     <tr>
-      <th scope="row">Size</th>
+      <th scope="row">Genome size:</th>
       <td>{{ annotation.stats.size }}</td>
     </tr>
     <tr>
-      <th scope="row">GC Ratio</th>
+      <th scope="row">GC:</th>
       <td>{{ ratioToPercentage(annotation.stats.gc) }}</td>
     </tr>
     <tr>
-      <th scope="row">n Ratio</th>
+      <th scope="row">N ratio:</th>
       <td>
         <NRatio :value="annotation.stats.n_ratio"></NRatio>
       </td>
     </tr>
     <tr>
-      <th scope="row">Coding Ratio</th>
+      <th scope="row">Coding ratio:</th>
       <td>
         {{ ratioToPercentage(annotation.stats.coding_ratio) }}
       </td>
