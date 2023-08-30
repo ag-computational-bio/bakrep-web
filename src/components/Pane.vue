6e16f4e4cb313495e1a44a232d6005345c8307a2
Noah Stürtz <noah.stuertz@gmail.com>
2023-08-30 15:47:36 +0200

    add system to name tabs freely

--- src/components/Pane.vue
+++ src/components/Pane.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-import Panetab from "./Panetab.vue";
+import type { Tab } from "@/model/Tabs";
+import PaneTab from "@/components/PaneTab.vue";
+import type { PropType } from "vue";
 
 type Action = {
   title: string;
   href: object;
 };
 
-let props = defineProps({
-  activeItem: {type: String},
-  items: { type: Array<string>, required: true },
-  action: { type: Object, required: true },
+defineProps({
+  activeItem: { type: String },
+  items: { type: Array as PropType<Array<Tab>>, required: true },
+  action: { type: Object as PropType<Action>, required: true },
 });
 
 const emits = defineEmits(["update:value"]);
@@ -19,15 +21,7 @@ const emits = defineEmits(["update:value"]);
   <!-- Tabbed Navigation -->
   <div class="shadow-sm p-4">
     <ul class="nav nav-pills py-3">
-      <Panetab
-        @click="
-          $emit('update:value', item);
-        "
-        :name="item"
-        :active="activeItem == item"
-        v-for="item in items"
-        :key="item"
-      />
+      <PaneTab @click="$emit('update:value', item.id)" :tab="item" :active="activeItem == item.id" v-for="item in items" :key="item.id" />
 
       <slot name="action">
         <li class="ms-auto" v-if="action">
---
+++ src/components/PaneTab.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Tab } from '@/model/Tabs';
+import type { PropType } from 'vue';
+
+defineProps({
+  tab: { type: Object as PropType<Tab>, required: true },
+  active: { type: Boolean, required: false },
+});
+</script>
+
+<template>
+  <li class="nav-item">
+    <a class="nav-link" :class="{ active }" href="#">{{ tab.title }}</a>
+  </li>
+</template>
