4a5ca467dc344aac03db1902132aed0171db5dee
Noah Stürtz <noah.stuertz@gmail.com>
2023-09-04 16:31:41 +0200

    add text based filter

--- src/components/QueryFilter.vue
+++ src/components/QueryFilter.vue
@@ -1,32 +1,68 @@
 <template>
-  <div class="rounded bg-body-tertiary p-4 mb-4">
-    <div>
-      <div><span class="fw-bold">Genome Size:</span> <span>{{ sizeValue[0] + 'kbp' }} - {{ sizeValue[1] + 'kbp' }}</span></div>
-      <vue-slider :min="0" :max="100000" v-model="sizeValue" :lazy="true" :interval="500" />
+  <div class="rounded bg-body-secondary p-4 mb-4">
+    <div class="input-group mb-3">
+      <span class="input-label input-group-text">Size:</span>
+      <input class="form-control" type="text" v-model="sizeMin" />
+      <span class="input-group-text">Min</span>
+      <input class="form-control" type="text" v-model="sizeMax" />
+      <span class="input-group-text">Max</span>
     </div>
-    <div>
-      <div class="fw-bold">Contig Count:</div>
-      <vue-slider :min="1" :max="1000" v-model="contigValue" :lazy="true" />
+
+    <div class="input-group mb-3">
+      <span class="input-label input-group-text">Contig Count:</span>
+      <input class="form-control" type="text" v-model="contigMin" />
+      <span class="input-group-text">Min</span>
+      <input class="form-control" type="text" v-model="contigMax" />
+      <span class="input-group-text">Max</span>
     </div>
-    <div>
-      <div class="fw-bold">Quality:</div>
-      <vue-slider :min="0" :max="100" v-model="qualityValue" :lazy="true" />
+
+    <div class="input-group mb-3">
+      <span class="input-label input-group-text">GC Ratio:</span>
+      <input class="form-control" type="text" v-model="gcMin" />
+      <span class="input-group-text">Min</span>
+      <input class="form-control" type="text" v-model="gcMax" />
+      <span class="input-group-text">Max</span>
     </div>
-    <div>
-      <div class="fw-bold">Contamination:</div>
-      <vue-slider :min="0" :max="100" v-model="contaminationValue" :lazy="true" />
+
+    <div class="input-group mb-3">
+      <div class="input-label input-group-text">Quality:</div>
+      <input class="form-control" type="text" v-model="qualityMin" />
+      <div class="input-group-text">Min</div>
+      <input class="form-control" type="text" v-model="qualityMax" />
+      <div class="input-group-text">Max</div>
+    </div>
+
+    <div class="input-group mb-3">
+      <div class="input-label input-group-text">Contamination:</div>
+      <input class="form-control" type="text" v-model="contaminationMin" />
+      <div class="input-group-text">Min</div>
+      <input class="form-control" type="text" v-model="contaminationMax" />
+      <div class="input-group-text">Max</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import VueSlider from 'vue-slider-component';
-import 'vue-slider-component/theme/default.css'
 
-const sizeValue = ref([0, 100000]);
-const contigValue = ref([1, 1000]);
-const qualityValue = ref([75, 100]);
-const contaminationValue = ref([0, 10]);
+const sizeMin = ref();
+const sizeMax = ref();
 
+const gcMin = ref();
+const gcMax = ref();
+
+const qualityMin = ref();
+const qualityMax = ref();
+
+const contaminationMin = ref();
+const contaminationMax = ref();
+
+const contigMin = ref();
+const contigMax = ref();
 </script>
+
+<style scoped>
+.input-label {
+  width: 8.5rem;
+}
+</style>
