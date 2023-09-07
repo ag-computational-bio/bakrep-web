7ad0f261f99405df9ae75dcdc45c3b0112faaca7
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2023-09-07 10:48:30 +0200

    Clip all whitespace from cb-logo

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,34 @@
 import { RouterLink, RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
-import { ref } from 'vue';
+import { ref } from "vue";
 import { onMounted } from "vue";
 
-const theme = ref(window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light')
-const element = document.getElementById("root")
+const theme = ref(
+  window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light",
+);
+const element = document.getElementById("root");
 
 onMounted(() => {
   if (element) {
-    element.dataset.bsTheme = theme.value
+    element.dataset.bsTheme = theme.value;
   }
-})
-
+});
 </script>
 
 <template>
   <Navbar />
 
-  <div id="content" class="mt-5 min-vh-100 pt-5">
+  <div id="content" class="">
     <RouterView />
   </div>
 
-  <Footer class="mt-5" />
-
+  <Footer class="" />
 </template>
 
 <style>
+#content {
+  margin-top: calc(80px + 1rem);
+  min-height: calc(100vh - 200px - 80px - 1rem);
+}
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
-    <footer class="py-3 my-3">
+    <footer class="">
       <div class="d-flex flex-column flex-xl-row justify-content-center">
-        <a href="https://www.uni-giessen.de/de/fbz/fb08/Inst/bioinformatik/" target="_blank">
+        <a
+          href="https://www.uni-giessen.de/de/fbz/fb08/Inst/bioinformatik/"
+          target="_blank"
+        >
           <img src="@/assets/images/CB.AG_Logo.svg" height="200" width="350" />
         </a>
         <a href="">
