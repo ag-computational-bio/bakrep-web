4ff4dd98fe3e1b0adbf52d9dfad607beae1f6963
Noah Stürtz <noah.stuertz@gmail.com>
2023-10-11 11:49:36 +0200

    draft

---
+++ src/components/RepositoryStatistics.vue
@@ -0,0 +1,24 @@
+<template>
+  <table class="statstable">
+    <tr>
+      <th scope="row">Datasets:</th>
+      <td>{{ datasets }}</td>
+    </tr>
+    <tr>
+      <th scope="row">Genera</th>
+      <td>{{ genera }}</td>
+    </tr>
+    <tr>
+      <th scope="row">Species</th>
+      <td>{{ species }}</td>
+    </tr>
+  </table>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  datasets: { type: Number, required: true },
+  genera: { type: Number, required: true },
+  species: { type: Number, required: true },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RepositoryStatistics from "@/components/RepositoryStatistics.vue";
+</script>
 
 <template>
   <main class="container pt-5">
@@ -6,29 +8,45 @@
       <div
         class="col-sm-12 col-md-4 ml-4 py-2 px-2 bg-secondary rounded text-light text-center"
       >
-        <h4>Statistics</h4>
-        <div>Lorem ipsum dolor sit amet.</div>
-
-        <div class="bg-light">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-          dolores optio incidunt ducimus cupiditate aut vel porro ratione atque.
-          Tempore consequatur cum fuga modi quam, distinctio sunt assumenda
-          obcaecati natus!
-        </div>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod,
-          veritatis!
-        </div>
+        <h4>Repository Statistics</h4>
+        <RepositoryStatistics
+          :datasets="661383"
+          :genera="2467"
+          :species="8208"
+        />
       </div>
       <div class="col-8 px-4">
         <h2>BakRep</h2>
-                The large amount of bacterial genomic data in public genome databases is an important resource for research in various fields. However, most of this data has been processed differently, making accurate comparisons challenging. <em>Blackwell et al</em>., used a uniform approach to assemble and characterise 661,405 bacterial genomes retrieved from the European Nucleotide Archive (ENA) in November of 2018. We build up on this resource and further analyse the assembled genomes in a standardised manner. We conducted a robust taxonomic classification using the Genome Taxonomy Database (GTDB) and furthermore subtyped all eligible genomes via multilocus-sequence typing. In addition we annotated all genomes assigning functional categories, e.g. COG and E.C., and database cross references to public databases. The overarching goal is to make this standardised resource available to non-bioinformaticians via an interactive website. This website will provide researchers with a flexible search engine to query the repository.            </div>
-        </div>
-        <div class="row py-4">            
-            <div class="col btn btn-primary me-5 py-2" @click="$router.push('browse')">Browse</div>
-            <div class="col btn btn-light ms-5 py-2" @click="$router.push('search')">Search</div>
-        </div>
-    </main>
+        The large amount of bacterial genomic data in public genome databases is
+        an important resource for research in various fields. However, most of
+        this data has been processed differently, making accurate comparisons
+        challenging. <em>Blackwell et al</em>., used a uniform approach to
+        assemble and characterise 661,405 bacterial genomes retrieved from the
+        European Nucleotide Archive (ENA) in November of 2018. We build up on
+        this resource and further analyse the assembled genomes in a
+        standardised manner. We conducted a robust taxonomic classification
+        using the Genome Taxonomy Database (GTDB) and furthermore subtyped all
+        eligible genomes via multilocus-sequence typing. In addition we
+        annotated all genomes assigning functional categories, e.g. COG and
+        E.C., and database cross references to public databases. The overarching
+        goal is to make this standardised resource available to
+        non-bioinformaticians via an interactive website. This website will
+        provide researchers with a flexible search engine to query the
+        repository.
+      </div>
+    </div>
+    <div class="row py-4">
+      <div
+        class="col btn btn-primary me-5 py-2"
+        @click="$router.push('browse')"
+      >
+        Browse
+      </div>
+      <div class="col btn btn-light ms-5 py-2" @click="$router.push('search')">
+        Search
+      </div>
+    </div>
+  </main>
 </template>
 
 <style></style>
