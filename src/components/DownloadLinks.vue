1b95ec0dfd6647ce96fda35c0380c389b52bdfd7
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2023-10-24 12:51:36 +0200

    Add fixed order on download page

--- src/components/DownloadLinks.vue
+++ src/components/DownloadLinks.vue
@@ -1,12 +1,13 @@
 <template>
   <ul
-    v-for="(group, key) of groups"
+    v-for="group of groups"
+    :key="group.key"
     class="fw-bold text-secondary"
     style="list-style-type: none"
   >
     <li>
-      {{ key }}
-      <ul v-for="r of group" class="fw-normal">
+      {{ group.key }}
+      <ul v-for="r of group.value" :key="r.url" class="fw-normal">
         <a :href="r.url" target="_blank" download>
           {{ getUrlTitle(r.url) }}
         </a>
@@ -18,6 +19,7 @@
 <script setup lang="ts">
 import type { Dataset, Result } from "@/model/Dataset";
 import { computed, type PropType } from "vue";
+import sortByOrder from "@/util/sort";
 
 const props = defineProps({
   dataset: { type: Object as PropType<Dataset> },
@@ -28,15 +30,27 @@ function getUrlTitle(url: string): string {
   return parts[parts.length - 1];
 }
 
+const order = ["assembly", "qc", "annotation", "taxonomy"].reduce(
+  (a: Record<string, number>, v: string, i: number) => {
+    a[v] = i;
+    return a;
+  },
+  {},
+);
+
 const groups = computed(() => {
   const o: Record<string, Result[]> = {};
   if (props.dataset) {
     props.dataset.results.forEach((r) => {
-      const type = r.attributes.type;
+      const type = r.attributes.type ? r.attributes.type : "other";
       if (!(type in o)) o[type] = [];
-      o[r.attributes.type].push(r);
+      o[type].push(r);
     });
   }
-  return o;
+
+  const out = Object.entries(o).map((e) => ({ key: e[0], value: e[1] }));
+  out.sort(sortByOrder(order, (x) => x.key));
+
+  return out;
 });
 </script>
