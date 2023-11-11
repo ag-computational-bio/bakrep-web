0dc77e1148ccb6868224dc2cbb924c495a9b0aff
Noah Stuertz <noah@coru.sh>
2023-11-11 20:07:25 +0100

    added environmental subpoints

---
+++ src/components/EnvironmentMetadata.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { PropType } from "vue";
+import { type Environment } from "@/model/Environment";
+
+defineProps({
+  environment: { type: Object as PropType<Environment>, required: true },
+});
+</script>
+
+<template>
+  <div class="container-full py-2">
+    <div>
+      <div class="fw-bold">Biome:</div>
+      <div>{{ environment.biome }}</div>
+    </div>
+    <div>
+      <div class="fw-bold">Feature:</div>
+      <div>{{ environment.feature }}</div>
+    </div>
+    <div>
+      <div class="fw-bold">Material:</div>
+      <div>
+        {{ environment.material }}
+      </div>
+    </div>
+  </div>
+</template>
